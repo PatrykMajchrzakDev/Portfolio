efdb1be06520916c5bf56e716b2f3a6226627dc9
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-04-04 19:31:58 +0200

    Added top navigation to root layout

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -3,11 +3,12 @@ import { Outlet } from "react-router-dom";
 
 // ============= Import Components =============
 import SideNavigation from "@/components/navigation/SideNavigation";
-import "./scss/Root.scss";
+import TopNavigation from "@/components/navigation/TopNavigation";
 
 const Root = () => {
   return (
-    <div className="test">
+    <div>
+      <TopNavigation />
       <SideNavigation />
       <Outlet />
     </div>
