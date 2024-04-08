a64c23f6e0e1db78afd0303ef9473d39212bd520
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-04-08 18:33:05 +0200

    Updated homepage to include hero section

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,14 @@
+// ============= Import Modules =============
+
+// ============= Import Components =============
+import Hero from "../components/hero/Hero";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Homepage;
