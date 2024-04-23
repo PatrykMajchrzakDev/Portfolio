79e8198743b396ac017c2481bce8ddb11f86096f
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2024-04-23 18:49:31 +0200

    Added language toggler component and moved togglers to separate li items

--- src/components/navigation/TopNavigation.tsx
+++ src/components/navigation/TopNavigation.tsx
@@ -4,7 +4,8 @@ import styles from "./scss/TopNavigation.module.scss";
 import { NavigationScrollContext } from "@/context/NavigationScroll";
 
 // ============= Import Components =============
-import ThemeToggler from "../UI/themeToggler/ThemeToggler";
+import ThemeToggler from "../UI/ThemeToggler/ThemeToggler";
+import LanguageToggler from "@/components/UI/LanguageToggler/LanguageToggler";
 import hamburgerMenu from "/assets/icons/bars-solid.svg";
 import xMark from "/assets/icons/xmark-solid.svg";
 
@@ -71,6 +72,7 @@ const TopNavigation = () => {
       {/* Hamburger Menu */}
       <div className={styles.hamburgerContainer}>
         <div className={styles.hamburgerWrapper}>
+          <LanguageToggler />
           <ThemeToggler id="1" />
           <div className={styles.hamburger}>
             {isMenuOpen ? (
@@ -88,8 +90,13 @@ const TopNavigation = () => {
 
       {/* DESKTOP VIEWPORT - Right navigation section */}
       <div className={styles.list}>
-        <ThemeToggler id="2" />
         <ul>
+          <li>
+            <LanguageToggler />
+          </li>
+          <li>
+            <ThemeToggler id="2" />
+          </li>
           <li>
             <a
               onClick={() => {
